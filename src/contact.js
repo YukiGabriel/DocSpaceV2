9c7aa46694ea3202dc5afcdad91e1911e79f9bbe
Yuki Gabriel <yukigabriell@gmail.com>
2026-02-26 12:11:10 -0300

    feat: add about, articles, and contact pages with responsive sidebar functionality

---
+++ src/contact.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelectorAll('#menu-button');
+const sidebar = document.getElementById('sidebar');
+
+menuButton.forEach(button => {
+  button.addEventListener('click', () => {
+    sidebar.classList.toggle('-translate-x-full');
+  });
+});
